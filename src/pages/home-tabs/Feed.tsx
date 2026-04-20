9130a8156606071d45a08cfc636781c7ed545bbc
lowPrio12 <ninar.ranin@gmail.com>
2026-04-20 08:14:54 +0800

    Content in feed

--- src/pages/home-tabs/Feed.tsx
+++ src/pages/home-tabs/Feed.tsx
@@ -1,6 +1,14 @@
-import { IonButtons, IonContent, IonHeader, IonMenu, IonMenuButton, IonPage, IonTitle, IonToolbar } from "@ionic/react"
+import { IonButtons, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonCol, IonContent, IonGrid, IonHeader, IonItem, IonLabel, IonList, IonMenu, IonMenuButton, IonPage, IonRow, IonTitle, IonToolbar } from "@ionic/react"
 
 const Feed: React.FC = () => {
+
+    const AncientHeroes = [
+        {name: 'Zeus'},
+        {name: 'Anti Mage'},
+        {name: 'Phantom Assassin'},
+        {name: 'Queen of Pain'},
+        {name: 'Shadow Fiend'},
+    ]
   return (
     <IonPage>
         <IonHeader>
@@ -12,7 +20,21 @@ const Feed: React.FC = () => {
                 
             </IonToolbar>
         </IonHeader>
-        <IonContent fullscreen>
+            <IonContent className = "ion-padding">
+            <IonList inset = {true}>
+
+            {AncientHeroes.map((item, index) => (
+    <IonCard>
+      <img alt="Silhouette of mountains" src="https://ionicframework.com/docs/img/demos/card-media.png" />
+      <IonCardHeader>
+        <IonCardTitle>{item.name}</IonCardTitle>
+        <IonCardSubtitle>Card Subtitle</IonCardSubtitle>
+      </IonCardHeader>
+
+      <IonCardContent>Here's a small text description for the card content. Nothing more, nothing less.</IonCardContent>
+    </IonCard>
+    ))}
+    </IonList>
         </IonContent>
     </IonPage>
     );   
